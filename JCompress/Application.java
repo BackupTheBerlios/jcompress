9dd6ea2ed29271df3f0f00a78d8ccbfd8e5c02f4
whisperine <whisperine>
2005-01-24 07:46:48 +0000

    algos compress decompress

--- JCompress/Application.java
+++ JCompress/Application.java
@@ -16,6 +16,14 @@ import javax.swing.filechooser.FileFilter;
  * @author Bosse Laure/Fauroux claire
  *	
  */
+
+/**
+ * *
+ * 
+ * @param
+ * @return
+ *  
+ */
 public class Application {
 
 	public Application() {
@@ -31,18 +39,79 @@ public class Application {
 		//ab.ajoutCaractere("test");
 		//ab.afficherListe();
 
+		/*String test = "a";
+		byte[] tab = test.getBytes();
+		for (int i=0; i< tab.length;i++)
+			System.out.println(tab[i]);
+			*/
 		init();
+	
 
 	}
 
 	public static void compresser() {
 
 		System.out.println("clic compress");
+		
+		//choix des fichiers
 		String fic = ouvrirFichier(".txt");
+		String ficDest = ouvrirFichier(".jcomp");
+
+		System.out.println("source :" + fic);
+		System.out.println("destination :" + ficDest);
+		
+		
+		
+		/*
+		 Ressources fichiers = new Ressources(fic, ficDest);
+		 ArbreBinaire arbre = new Arbre();
+		 
+		 //tq pas fin de fichier
+		 for (String car = fichiers.lireOctet();car!= ArbreBinaire.EOF;
+		 		car = fichiers.lireOctet()){
+		 {
+		 	Noeud n = getNoeud(c);
+		 	if (n==null)
+		 	{
+		 			byte[] tab = car.getBytes();
+		 			//nouveau caractere
+		 			fichiers.ecrireCaractere(getNoeud(ArbreBinaire.ECHAP).getCodeDansArbreBinaire());
+		 			fichiers.ecrireCaractere(tab[0]);
+		 	}
+		 	else
+		 	{
+		 		//caractere redondant
+		 		fichiers.ecrireCaractere(n.getCodeDansArbreBinaire());	
+		 	}
+		 	arbre.ajoutCaractere(car);
+		 }
+		 //rajout de EOF dans arbre???
+		 ressources.fermer();
+		 System.out.println("compression terminée");
+		 
+		 */
+
 	}
 
 	public static void decompresser() {
 		System.out.println("clic decompress");
+		
+		//choix des fichiers
+		String fic = ouvrirFichier(".jcomp");
+		String ficDest = ouvrirFichier(".txt");
+
+		System.out.println("source :" + fic);
+		System.out.println("destination :" + ficDest);
+		
+		/*Ressources fichiers = new Ressources(fic, ficDest);
+		 ArbreBinaire arbre = new Arbre();
+		 BitSet bits = new BitSet();
+		 
+		 for (int bit = fichiers.lireBit();
+		 	bit != -1;	bit= fichiers.lireBit())
+		 
+		*/
+
 	}
 
 	/**
@@ -75,39 +144,63 @@ public class Application {
 		fenetre.setVisible(true);
 	}
 
-	private static String ouvrirFichier(final String extension ){
-		
-		JFileChooser chooser  = new JFileChooser();
-	    FileFilter filter = new FileFilter(){
+	private static String ouvrirFichier(final String extension) {
+
+		JFileChooser chooser = new JFileChooser();
+		FileFilter filter = new FileFilter() {
 			public boolean accept(File f) {
-				//marche pas pour les txt sai pas pkoi
 				//recupere l'extension
-				int index =f.getAbsolutePath().lastIndexOf('.');
-				String ext="";
-				if (index!=-1)
-						ext = f.getAbsolutePath().substring(index);
-				else return true;
-				System.out.println("ext : "+ext);
-				System.out.println("extension : "+extension);
-				if (ext == extension)
+				int index = f.getAbsolutePath().lastIndexOf('.');
+				String ext = "";
+				if (index != -1)
+					ext = f.getAbsolutePath().substring(index);
+				else
+					//directories
 					return true;
-				else return false;
+				if (ext.compareTo(extension) == 0) {
+					return true;
+				} else
+					return false;
 			}
+
 			public String getDescription() {
-				// TODO Auto-generated method stub
 				return extension;
 			}
-	    };
-	    //filter.addExtension("jpg");
-	    //filter.addExtension("gif");
-	    //filter.setDescription("JPG & GIF Images");
-	    chooser.setFileFilter(filter);
-	    int returnVal = chooser.showOpenDialog(new JFrame("open"));
-	    if(returnVal == JFileChooser.APPROVE_OPTION) {
-	       System.out.println("file: " +
-	            chooser.getSelectedFile().getName());
-	    }
-		
+		};
+		chooser.setFileFilter(filter);
+		int returnVal = chooser.showOpenDialog(new JFrame("open"));
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+
+			System.out.println("file: " + chooser.getSelectedFile().getName());
+			//contains vient de java 5.0
+			/*if (chooser.getSelectedFile().getName().contains(
+					new CharSequence() {
+						public int length() {
+							// TODO Auto-generated method stub
+							return extension.length();
+						}
+
+						public char charAt(int index) {
+							// TODO Auto-generated method stub
+							return extension.charAt(index);
+						}
+
+						public CharSequence subSequence(int start, int end) {
+							// TODO Auto-generated method stub
+							return null;
+						}
+
+						public String toString() {
+							// TODO Auto-generated method stub
+							return extension;
+						}
+					}))
+			*/
+				return chooser.getSelectedFile().getAbsolutePath();
+			/*else {
+				return chooser.getSelectedFile().getAbsolutePath() + extension;
+			}*/
+		}
 		return null;
 	}
 }
