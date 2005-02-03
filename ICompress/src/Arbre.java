4bfe9290f92894f840f81f68021e609b5154d679
whisperine <whisperine>
2005-02-03 19:54:27 +0000

    creation

---
+++ ICompress/src/Arbre.java
@@ -0,0 +1,18 @@
+
+package src;
+
+/**
+ * @author claire
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Arbre {
+	
+	Noeud racine = null;
+
+	
+
+	public static void main(String[] args) {
+	}
+}
