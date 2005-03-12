63c5c4888088256e49739e2e301e234157a41aa7
whisperine <whisperine>
2005-03-12 13:48:51 +0000

    *** empty log message ***

---
+++ OLAPSQL/src/Appli.java
@@ -0,0 +1,191 @@
+
+package src;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import javaCC.Analyzer;
+import javaCC.ParseException;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.filechooser.FileFilter;
+
+import structure.Commande;
+
+/**
+ * @author claire
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Appli {
+
+    private static String NEW_LINE = "\n";
+
+    private static JFrame jFrame;
+	private static JTextArea textArea;
+	private static JButton boutonOuvrir;
+	private static String chemin ="T:\\IUP Master 1\\sem2\\BOT\\projet3 BOT";
+	private JTable table;
+	static Analyzer parser=null;
+    
+	
+	
+    /**
+     */
+    public Appli(){
+    }
+    
+    public static void main(String[] args) {
+        init();
+    }
+
+    /**
+     * 
+     */
+    private static void init() {
+		// Initialisation de la fenetre
+		jFrame = new JFrame("OLAPSQL");
+		jFrame.setSize(500, 310);
+
+		// Affichage de la fenetre au centre de l'ecran
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		jFrame.setLocation(dim.width / 2 - jFrame.getWidth() / 2, dim.height
+				/ 2 - jFrame.getHeight() / 2);
+
+		// Permet la cloture de l'application
+		jFrame.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+				System.exit(0);
+			}
+		});
+		
+		//Bouton OuvrirFichier
+		boutonOuvrir = new JButton("requete OLAPSQL");
+		boutonOuvrir.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				String fichierSource = ouvrirFichier(".olapsql");
+				if(fichierSource != null){
+				    System.out.println(fichierSource);
+				    //TODO effacer le textArea
+				    analyser(fichierSource);
+				}
+				else
+				    textArea.append("Fichier inconnu." + NEW_LINE);
+			}});
+		// Champ texte pour prevenir l'utilisateur
+		textArea = new JTextArea(5, 20);
+		textArea.setEditable(false);
+		JScrollPane jFiel = new JScrollPane(textArea);
+		jFiel.setPreferredSize(new Dimension(250, 100));
+		
+		//Ajout des elements a la fenetre
+		FlowLayout layout = new FlowLayout(FlowLayout.CENTER);
+		jFrame.getContentPane().setLayout(layout);
+		jFrame.getContentPane().add(new JLabel("LOG:"));
+		jFrame.getContentPane().add(textArea,BorderLayout.NORTH);
+		jFrame.getContentPane().add(boutonOuvrir,BorderLayout.CENTER);
+		
+		
+		//Rend visible la fenetre
+		jFrame.setVisible(true);
+    }
+    
+    /**
+     * @param fichierSource, nom du fichier contenant requetes
+     */
+    protected static void analyser(String fichierSource) {
+
+        try {
+			FileReader f = new FileReader(fichierSource);
+			if (parser == null)
+			{
+				parser = new Analyzer (f);
+			}
+			else
+			{
+				parser.ReInit(f);
+			}
+			try {
+				Commande c = parser.execute();
+				c.afficher();
+				System.out.println("ok");
+			} catch (ParseException e) {
+				System.out.println("Erreur de syntaxe OLAPSQL");
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+    }
+
+    /**
+	 * Ouvre une fenetre permettant de choisir un fichier ayant pour extension
+	 * le parametre.
+	 * @param extension Type d'extension (exemple : ".txt").
+	 * @return Chemin du ficheir choisi par l'utilisateur
+	 */
+	private static String ouvrirFichier(final String extension){
+
+		JFileChooser chooser = new JFileChooser();
+		//chemin
+		chooser.setCurrentDirectory(new File(chemin));
+		// titre
+		chooser.setDialogTitle("test");
+		FileFilter filter = new FileFilter(){
+			public boolean accept(File f){
+				//recupere l'extension
+				int index = f.getAbsolutePath().lastIndexOf('.');
+				String ext = "";
+				if(index != -1)
+					ext = f.getAbsolutePath().substring(index);
+				else
+					//directories
+					return true;
+				return (ext.compareTo(extension) == 0);
+			}
+
+			public String getDescription(){
+				return extension;
+			}
+		};
+		chooser.setFileFilter(filter);
+		int returnVal = chooser.showOpenDialog(new JFrame("open"));
+		if(returnVal == JFileChooser.APPROVE_OPTION){
+
+			System.out.println("file: " + chooser.getSelectedFile().getName());
+			return chooser.getSelectedFile().getAbsolutePath();
+		}
+		return null;
+	}
+	
+	
+    /**
+     * @return Returns the table.
+     */
+    public JTable getTable() {
+        return table;
+    }
+    /**
+     * @param table The table to set.
+     */
+    public void setTable(JTable table) {
+        this.table = table;
+    }
+}
