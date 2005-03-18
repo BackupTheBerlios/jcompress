e6edaa75c27c455a3a161f817ca82460d35928eb
whisperine <whisperine>
2005-03-18 13:09:07 +0000

    maj

--- OLAPSQL/structure/InsertFact.java
+++ OLAPSQL/structure/InsertFact.java
@@ -7,6 +7,10 @@
 package structure;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
+import structure.types.predicat.Predicat;
 
 /**
  * @author m1isi17
@@ -19,8 +23,7 @@ public class InsertFact extends Insert{
 
 	//cle ; String nom du connect
 	//valeur : Predicat
-	private HashMap connects; 
-	
+	private HashMap connects = new HashMap(); 
 	
 	/**
 	 * @param nom
@@ -28,20 +31,36 @@ public class InsertFact extends Insert{
 	 */
 	public InsertFact(String nom, int type) {
 		super(nom, type);
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @return Retourne la valeur de l'attribut connects.
-	 */
-	public HashMap getConnects(){
-		return connects;
 	}
 	
-	/**
-	 * @param initialse connects avec pConnects.
-	 */
-	public void setConnects(HashMap pConnects){
-		connects = pConnects;
+	public InsertFact(String nom, int type, String premValue) {
+		super(nom, type);
+		ajoutValeur(premValue);
 	}
+	
+	public void ajoutConnect (String nomC, Predicat p){
+		connects.put(nomC, p);
+	}
+	
+	/* (non-Javadoc)
+	 * @see structure.Commande#afficher()
+	 */
+	public void afficher() {
+		super.afficher();
+		afficherConnects();
+	}
+	
+	public void afficherConnects (){
+		
+		Set keys=connects.keySet();
+		//Iterator it = keys.iterator();
+		
+		for (Iterator it = keys.iterator(); it.hasNext();)
+		{
+				String key = (String)it.next();
+				System.out.println("connect : key "+key+ " value");
+				((Predicat)(connects.get(key))).afficher();
+		}
+	}
+
 }
--- OLAPSQL/structure/Select.java
+++ OLAPSQL/structure/Select.java
@@ -8,6 +8,7 @@ package structure;
 
 import java.util.ArrayList;
 
+import structure.types.Attribut;
 import structure.types.predicat.Predicat;
 
 /**
@@ -18,25 +19,185 @@ import structure.types.predicat.Predicat;
  */
 public class Select extends Commande{
 
-	private String nomRow;
-	private String withRow;
-	private ArrayList rows;
+	//liste de String  : "fonc(att)"
+	private ArrayList agregs = new ArrayList();
 	
-	private String nomColumn;
-	private String withColumn;
-	private ArrayList columns;
+	private String nomRow="";
+	private String withRow="";
+	//liste de String : nom[.nom]?
+	private ArrayList rows=new ArrayList();
 	
-	private String nomFrom;
-	private Predicat where;
+	private String nomColumn="";
+	private String withColumn="";
+	private ArrayList columns = new ArrayList();
 	
+	private String nomFrom="";
+	private Predicat where=null;
+	
+	
+	
+	public Select(String nom, int type){super("",-1);}
 	/**
-	 * @param nom
-	 * @param type
+	 * @param nom = ""
+	 * @param type = -1
 	 */
-	public Select(String nom, int type) {
-		super(nom, type);
-		// TODO Auto-generated constructor stub
+	public Select(ArrayList l) {
+		super("",-1);
+		agregs = l;
 	}
 
+	
+	
+	/* (non-Javadoc)
+	 * @see structure.Commande#afficher()
+	 */
+	public void afficher() {
+		super.afficher();
+		
+		System.out.println("liste d'aggregs");
+		affString(agregs);
+		
+		System.out.println("RoW :" +nomRow);
+		if (withRow !="")
+		{
+			System.out.println("with " +withRow);
+			affString(rows);
+		}
+		System.out.println("Column :" +nomColumn);
+		if (withColumn !="")
+		{
+			System.out.println("with " +withColumn);
+			affString(columns);
+		}
+		
+		System.out.println("FROM " +nomFrom);
+		if (where !=null)where.afficher();
+		
+	}
+	
+	public void affString(ArrayList l)
+	{
+		for (int i=0; i<l.size();i++)
+		{
+				System.out.println(i+" : "+(String)l.get(i));
+		}
+	}
+	
+	
+	public void ajoutRow(String s){
+		rows.add(s);
+	}
 
+	public void ajoutColumn(String s){
+		columns.add(s);
+	}
+	
+	/**
+	 * @return Returns the agregs.
+	 */
+	public ArrayList getAgregs() {
+		return agregs;
+	}
+	/**
+	 * @param agregs The agregs to set.
+	 */
+	public void setAgregs(ArrayList agregs) {
+		this.agregs = agregs;
+	}
+	/**
+	 * @return Returns the columns.
+	 */
+	public ArrayList getColumns() {
+		return columns;
+	}
+	/**
+	 * @param columns The columns to set.
+	 */
+	public void setColumns(ArrayList columns) {
+		this.columns = columns;
+	}
+	/**
+	 * @return Returns the nomColumn.
+	 */
+	public String getNomColumn() {
+		return nomColumn;
+	}
+	/**
+	 * @param nomColumn The nomColumn to set.
+	 */
+	public void setNomColumn(String nomColumn) {
+		this.nomColumn = nomColumn;
+	}
+	/**
+	 * @return Returns the nomFrom.
+	 */
+	public String getNomFrom() {
+		return nomFrom;
+	}
+	/**
+	 * @param nomFrom The nomFrom to set.
+	 */
+	public void setNomFrom(String nomFrom) {
+		this.nomFrom = nomFrom;
+	}
+	/**
+	 * @return Returns the nomRow.
+	 */
+	public String getNomRow() {
+		return nomRow;
+	}
+	/**
+	 * @param nomRow The nomRow to set.
+	 */
+	public void setNomRow(String nomRow) {
+		this.nomRow = nomRow;
+	}
+	/**
+	 * @return Returns the rows.
+	 */
+	public ArrayList getRows() {
+		return rows;
+	}
+	/**
+	 * @param rows The rows to set.
+	 */
+	public void setRows(ArrayList rows) {
+		this.rows = rows;
+	}
+	/**
+	 * @return Returns the where.
+	 */
+	public Predicat getWhere() {
+		return where;
+	}
+	/**
+	 * @param where The where to set.
+	 */
+	public void setWhere(Predicat where) {
+		this.where = where;
+	}
+	/**
+	 * @return Returns the withColumn.
+	 */
+	public String getWithColumn() {
+		return withColumn;
+	}
+	/**
+	 * @param withColumn The withColumn to set.
+	 */
+	public void setWithColumn(String withColumn) {
+		this.withColumn = withColumn;
+	}
+	/**
+	 * @return Returns the withRow.
+	 */
+	public String getWithRow() {
+		return withRow;
+	}
+	/**
+	 * @param withRow The withRow to set.
+	 */
+	public void setWithRow(String withRow) {
+		this.withRow = withRow;
+	}
 }
